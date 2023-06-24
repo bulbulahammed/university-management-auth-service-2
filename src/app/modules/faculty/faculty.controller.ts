9fe8f055fa44552535ae1d2448832a20cd7c50fb
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-06-24 14:19:26 +0600

    [DONE] Get All Faculties

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicFacultyFilterableFields } from './faculty.constant';
 import { IAcademicFaculty } from './faculty.interface';
 import { AcademicFacultyService } from './faculty.service';
 
@@ -19,6 +22,26 @@ const createFaculty = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+// Get All Faculty Controller
+const getAllFaculties = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, academicFacultyFilterableFields);
+  const paginationOptions = pick(req.query, paginationFields);
+
+  const result = await AcademicFacultyService.getAllFaculties(
+    filters,
+    paginationOptions
+  );
+
+  sendResponse<IAcademicFaculty[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculties retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const AcademicFacultyController = {
   createFaculty,
+  getAllFaculties,
 };
--- src/app/modules/faculty/faculty.service.ts
+++ src/app/modules/faculty/faculty.service.ts
@@ -1,4 +1,9 @@
-import { IAcademicFaculty } from './faculty.interface';
+import { SortOrder } from 'mongoose';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/paginations';
+import { academicFacultySearchableFields } from './faculty.constant';
+import { IAcademicFaculty, IAcademicFacultyFilters } from './faculty.interface';
 import { AcademicFaculty } from './faculty.model';
 
 // Create User Service
@@ -9,6 +14,63 @@ const createFaculty = async (
   return result;
 };
 
+// Get All Faculties
+
+const getAllFaculties = async (
+  filters: IAcademicFacultyFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IAcademicFaculty[]>> => {
+  const { searchTerm, ...filtersData } = filters;
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: academicFacultySearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await AcademicFaculty.find(whereConditions)
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await AcademicFaculty.countDocuments();
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AcademicFacultyService = {
   createFaculty,
+  getAllFaculties,
 };
